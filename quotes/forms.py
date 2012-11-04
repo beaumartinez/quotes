55767ad722978d10e471f25a060def1f6115c253
Beau <beau@beaumartinez.com>
2012-11-04 14:07:33 +0000

    Add forms.py

---
+++ quotes/forms.py
@@ -0,0 +1,50 @@
+from django.forms import ModelForm
+from django.forms import CharField
+from django.forms import Textarea
+
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout
+from crispy_forms.layout import Submit
+
+from quotes.models import Author
+from quotes.models import Quote
+from quotes.models import Source
+
+class QuoteForm(ModelForm):
+
+    class Meta(object):
+        model = Quote
+
+        exclude = (
+            'user',
+        )
+
+    def __init__(self, request, *args, **kwargs):
+        self.request = request
+
+        super(QuoteForm, self).__init__(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        commit = kwargs.pop('commit', True)
+
+        kwargs['commit'] = False
+
+        quote = super(QuoteForm, self).save(*args, **kwargs)
+        quote.user = self.request.user
+
+        if commit:
+            quote.save()
+
+        return quote
+
+    helper = FormHelper()
+    helper.layout = Layout(
+        'content',
+        'author',
+        'source',
+
+        FormActions(
+            Submit('create', 'Create quote', css_class='btn-primary'),
+        )
+    )
