664fe8708d49c1b6a8f3e9e7c4d74617640d421b
Beau <beau@beaumartinez.com>
2012-11-04 14:06:16 +0000

    Add database

--- quotes/settings.py
+++ quotes/settings.py
@@ -2,3 +2,10 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ROOT_URLCONF = 'quotes.urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'quotes.db',
+    },
+}
