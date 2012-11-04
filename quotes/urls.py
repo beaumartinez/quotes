04b7979a70dc9b36dbe8af5564c4d2471b66aa2c
Beau <beau@beaumartinez.com>
2012-11-04 14:07:53 +0000

    Add create_quote view

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('quotes.views',
     url('^$', 'landing', name='landing'),
+    url('^quotes/create/$', 'create_quote', name='create_quote'),
 )
--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from quotes.forms import QuoteForm
 
 def landing(request):
     return HttpResponse('Hi')
+
+def create_quote(request):
+    form = QuoteForm(request.POST or None)
+
+    if form.is_valid():
+        quote = form.save()
+
+    return render(request, 'create_quote.html', {
+        'form': form, 
+    })
