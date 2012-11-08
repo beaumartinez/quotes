8086035dcf70b8e2e34801aefde8ab69ebe93dda
Beau <beau@beaumartinez.com>
2012-11-08 17:43:53 +0000

    We don't need no namespaced URL paths

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('quotes.views',
     url('^$', 'landing', name='landing'),
-    url('^quotes/create/$', 'create_quote', name='create_quote'),
-    url('^quotes/edit/(.+)$', 'edit_quote', name='edit_quote'),
-    url('^quotes/list/$', 'list_quotes', name='list_quotes'),
+    url('^create/$', 'create_quote', name='create_quote'),
+    url('^edit/(.+)$', 'edit_quote', name='edit_quote'),
+    url('^list/$', 'list_quotes', name='list_quotes'),
 )
