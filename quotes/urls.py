bec2eebc4e06d405cfd9f6b0abe7172e100ea91d
Beau <beau@beaumartinez.com>
2012-11-08 17:46:38 +0000

    OH SHIT NO TRAILING SLASH!!1

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('quotes.views',
     url('^$', 'landing', name='landing'),
     url('^create/$', 'create_quote', name='create_quote'),
-    url('^edit/(.+)$', 'edit_quote', name='edit_quote'),
+    url('^edit/(.+)/$', 'edit_quote', name='edit_quote'),
     url('^list/$', 'list_quotes', name='list_quotes'),
 )
