824dccdf533ac02570b42583a0cb29f22c42e75c
Beau <beau@beaumartinez.com>
2012-11-08 15:18:11 +0000

    Absract create_quote

--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,6 @@
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 from quotes.forms import QuoteForm
@@ -6,12 +8,17 @@ from quotes.forms import QuoteForm
 def landing(request):
     return HttpResponse('Hi')
 
-def create_quote(request):
-    form = QuoteForm(request.POST or None)
+def _create_or_edit_quote(request, quote=None):
+    form = QuoteForm(request, request.POST or None, instance=quote)
 
     if form.is_valid():
         quote = form.save()
 
+        return redirect(reverse('landing'))
+
     return render(request, 'create_quote.html', {
         'form': form, 
     })
+
+def create_quote(request):
+    return _create_or_edit_quote(request)
