1900bc41364f82efb0eeb5ac4f7a985f8c6b000c
Beau <beau@beaumartinez.com>
2012-11-21 20:57:42 +0000

    Sort URLs

--- quotes/api/urls.py
+++ quotes/api/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('quotes.api.views',
     url(r'^$', 'root', name='api.root'),
 
-    url(r'^quotes/$', 'list_quotes', name='api.list_quotes'),
     url(r'^authors/$', 'list_authors', name='api.list_authors'),
+    url(r'^quotes/$', 'list_quotes', name='api.list_quotes'),
     url(r'^sources/$', 'list_sources', name='api.list_sources'),
 
-    url(r'^quotes/(?P<pk>.+)/$', 'quote', name='api.quote'),
     url(r'^authors/(?P<pk>.+)/$', 'author', name='api.author'),
+    url(r'^quotes/(?P<pk>.+)/$', 'quote', name='api.quote'),
     url(r'^sources/(?P<pk>.+)/$', 'source', name='api.source'),
 )
