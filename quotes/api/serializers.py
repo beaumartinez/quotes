4f7d46e04a511e668366aa7f644256909ad1e90f
Beau <beau@beaumartinez.com>
2012-12-02 22:03:34 +0000

    Remove null fields in API
    Harder and hackier than it should be

--- quotes/api/serializers.py
+++ quotes/api/serializers.py
@@ -1,3 +1,6 @@
+from collections import Sequence
+from itertools import imap
+
 from rest_framework.serializers import HyperlinkedIdentityField
 from rest_framework.serializers import ManyHyperlinkedRelatedField
 from rest_framework.serializers import ModelSerializer
@@ -6,7 +9,23 @@ from quotes.models import Author
 from quotes.models import Quote
 from quotes.models import Source
 
-class AuthorSerializer(ModelSerializer):
+def _filter_none_values(dict_):
+    return {key: value for key, value in dict_.iteritems() if value is not None}
+
+class _NoNullFieldModelSerializer(ModelSerializer):
+
+    @property
+    def data(self):
+        data = super(_NoNullFieldModelSerializer, self).data
+
+        if isinstance(data, Sequence):
+            data = imap(lambda x: _filter_none_values(x), data)
+        else:
+            data = _filter_none_values(data)
+
+        return data
+
+class AuthorSerializer(_NoNullFieldModelSerializer):
 
     class Meta(object):
         model = Author
@@ -20,7 +39,7 @@ class AuthorSerializer(ModelSerializer):
     url = HyperlinkedIdentityField(view_name='api.author')
     quotes = ManyHyperlinkedRelatedField(view_name='api.quote')
 
-class SourceSerializer(ModelSerializer):
+class SourceSerializer(_NoNullFieldModelSerializer):
 
     class Meta(object):
         model = Source
@@ -34,7 +53,7 @@ class SourceSerializer(ModelSerializer):
     url = HyperlinkedIdentityField(view_name='api.source')
     quotes = ManyHyperlinkedRelatedField(view_name='api.quote')
 
-class QuoteSerializer(ModelSerializer):
+class QuoteSerializer(_NoNullFieldModelSerializer):
 
     class Meta(object):
         model = Quote
